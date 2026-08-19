043e26e27e9e1423bb835bea7aa4dd21a9af5ded
Qiuqiang Kong <qiuqiangkong@gmail.com>
2026-08-19 17:38:24 +0800

    update

--- README.md
+++ README.md
@@ -10,8 +10,13 @@ The dataset can be downloaded at xxx
 
 Users can also reproduce the dataset by running: 
 
+1. Download SoundFont 2 to produce audio from https://musical-artifacts.com/artifacts/738/FluidR3_GM.sf2
+
 ```bash
-wget sf
+$ git clone https://github.com/qiuqiangkong/mnist_av
+$ cd mnist_av
+$ uv sync
+$ source .venv/bin/activate
 ```
 
 ```python
--- create_dataset.py
+++ create_dataset.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 from pathlib import Path
@@ -9,7 +10,7 @@ import soundfile as sf
 from datasets import load_dataset
 from PIL import Image
 
-INSTS_MAP = {
+INSTRUMENT_MAP = {
     "piano":  "Acoustic Grand Piano",
     "violin": "Violin",
     "viola": "Viola",
@@ -23,37 +24,42 @@ INSTS_MAP = {
 }
 
 
-def create_dataset(split: str) -> None:
+def create_dataset(args) -> None:
+    """Create MNIST audio and movie dataset."""
+
+    sf2_path = Path(args.sf2_path)
+    out_dir = Path(args.out_dir)
+    split = args.split
 
     duration = 2.
-    video_fps = 12
+    video_fps = 24
     audio_sr = 16000
+    min_deg_per_sec = 1
     max_deg_per_sec = 180
     rs = np.random.RandomState(1234)
 
     dataset = load_dataset("mnist")
     imgs = dataset[split]["image"]
-    tgts = dataset[split]["label"]
+    labels = dataset[split]["label"]
     n_data = len(imgs)
     
     tmp_dir = Path("./_tmp")
-    root = Path("./dataset")
-
+    
     for n in range(n_data):
         # Video
         print(f"{n}/{n_data}")
         x = imgs[n].resize((32, 32), Image.BILINEAR)
-        speed = rs.uniform(-max_deg_per_sec, max_deg_per_sec)
+        speed = rs.uniform(min_deg_per_sec, max_deg_per_sec) * rs.choice([-1, 1])
         xs = rotate_mnist(x, duration, speed, video_fps)
 
         video_path = tmp_dir / split / "video_only" / f"{n:05d}.mp4"
         video_path.parent.mkdir(parents=True, exist_ok=True)
-        pil_to_video(xs, path=video_path, fps=video_fps)
+        pil_to_video(images=xs, path=video_path, fps=video_fps)
         print(f"Write to {video_path}")
 
         # Audio
-        inst_name = rs.choice(list(INSTS_MAP.keys()))
-        audio = create_audio(INSTS_MAP[inst_name], duration, speed, audio_sr)
+        inst_name = rs.choice(list(INSTRUMENT_MAP.keys()))
+        audio = create_audio(INSTRUMENT_MAP[inst_name], duration, speed, sf2_path, audio_sr)
 
         audio_path = tmp_dir / split / "audio_only" / f"{n:05d}.wav"
         audio_path.parent.mkdir(parents=True, exist_ok=True)
@@ -61,25 +67,24 @@ def create_dataset(split: str) -> None:
         print(f"Write to {audio_path}")
         
         # Audio & Video
-        out_path = root / split / "audio_video" / f"{n:05d}.mp4"
+        out_path = out_dir / split / "audio_video" / f"{n:05d}.mp4"
         out_path.parent.mkdir(parents=True, exist_ok=True)
         os.system(f'ffmpeg -y -loglevel error -i {video_path} -i {audio_path} -c:v copy -c:a aac {out_path}')
         print(f"Write to {out_path}")
 
         # Metadata
         meta = {
-            "digit": tgts[n],
+            "video_fps": video_fps,
+            "audio_sr": audio_sr,
+            "digit": labels[n],
             "speed": speed,
             "instrument": inst_name
         }
 
-        meta_path = root / split / "metadata" / f"{n:05d}.json"
+        meta_path = out_dir / split / "metadata" / f"{n:05d}.json"
         meta_path.parent.mkdir(parents=True, exist_ok=True)
-        write_jsonl(meta, meta_path)
+        write_json(meta, meta_path)
         print(f"Write to {meta_path}")
-
-        # if n == 100:
-        #     break
         
 
 def rotate_mnist(
@@ -99,7 +104,6 @@ def rotate_mnist(
     Returns:
         frames: (list[Image])
     """
-
     n_frames = int(duration * fps)
     frames = []
 
@@ -112,8 +116,7 @@ def rotate_mnist(
 
 
 def pil_to_video(images: list[Image], path: str, fps: int) -> None:
-    r"""Write images to video.
-    """
+    """Write images to video."""
     frames = [np.array(img.convert("RGB")) for img in images]
     h, w, _ = frames[0].shape
 
@@ -126,16 +129,14 @@ def pil_to_video(images: list[Image], path: str, fps: int) -> None:
     )
 
     for frame in frames:
-        # RGB -> BGR
-        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # RGB -> BGR
         writer.write(frame)
 
     writer.release()
 
 
-def create_audio(instrument_name: str, duration: float, speed: float, sr: float) -> np.ndarray:
-    r"""Create audio.
-    """
+def create_audio(instrument_name: str, duration: float, speed: float, sf2_path: str, sr: int) -> np.ndarray:
+    """Create audio."""
     midi = pretty_midi.PrettyMIDI()
 
     instrument = pretty_midi.Instrument(
@@ -147,11 +148,11 @@ def create_audio(instrument_name: str, duration: float, speed: float, sr: float)
     n_notes = len(onsets)
     
     if speed >= 0:
-        intervals = np.array([0, 2, 4, 5, 7, 9, 11])
-        intervals = np.concatenate([intervals, intervals + 12, intervals + 24], axis=0)
-    else:
         intervals = np.array([0, -1, -3, -5, -7, -8, -10])
         intervals = np.concatenate([intervals, intervals - 12, intervals - 24], axis=0)
+    else:
+        intervals = np.array([0, 2, 4, 5, 7, 9, 11])
+        intervals = np.concatenate([intervals, intervals + 12, intervals + 24], axis=0)
 
     intervals = intervals[0 : n_notes]
 
@@ -165,16 +166,22 @@ def create_audio(instrument_name: str, duration: float, speed: float, sr: float)
         instrument.notes.append(note)
     
     midi.instruments.append(instrument)
-    audio = midi.fluidsynth(sf2_path="FluidR3_GM.sf2", fs=sr)
+    audio = midi.fluidsynth(sf2_path=str(sf2_path), fs=sr)
     audio = audio[0 : int(duration * sr)]
     return audio
         
 
-def write_jsonl(meta: dict, path: str) -> None:
+def write_json(meta: dict, path: str) -> None:
     with open(path, "w", encoding="utf-8") as f:
         f.write(json.dumps(meta, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    create_dataset(split="train")
-    create_dataset(split="test")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--out_dir", type=str, default="./dataset")
+    parser.add_argument("--sf2_path", type=str, default="./FluidR3_GM.sf2")
+    args = parser.parse_args()
+
+    for split in ["train", "test"]:
+        args.split = split
+        create_dataset(args)
